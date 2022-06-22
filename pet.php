dc0a41f6a07787b1a7bad5f551c0a052aad4f4d1
OlyaLazareva <107986215+OlyaLazareva@users.noreply.github.com>
2022-06-22 11:16:25 +0300

    Update pet.php

--- pet.php
+++ pet.php
@@ -20,82 +20,82 @@ require "db.php";
 <body>
     <header class="header">
         <ul>  
-            <li><a href="index.php"><img class="header_logo" width="220" height="175" src="image/logo.png" alt="Логотип"></a></li>
-            <li><a href="menu.php"><img class="menu" src="image/menu.png" alt=""></a></li>
+            <li><a href="index.php"><img class="header_logo" width="220" height="175" src="../image/logo.png" alt="Логотип"></a></li>
+            <li><a href="menu.php"><img class="menu" src="../image/menu.png" alt=""></a></li>
         </ul>
     </header>
 
     <main class="main">
-        <img class="filter" src="image/filter.png" alt="">
+        <img class="filter" src="../image/filter.png" alt="">
         <h1>Питомцы</h1>
         <div class="Troy">
-            <img class="Troy_img" width="634" height="678" src="image/трой10.png" alt="">
+            <img class="Troy_img" width="634" height="678" src="../image/трой10.png" alt="">
             <div class="Troy_name"><p>Трой</p></div>
             <div class="Troy_age"><p>3 года</p></div>
         </div>
 
         <div class="Ava">
-            <img class="Ava_img" width="570" height="399" src="image/ава3.png" alt="">
+            <img class="Ava_img" width="570" height="399" src="../image/ава3.png" alt="">
             <div class="Ava_name"><p>Ава</p></div>
             <div class="Ava_age"><p>2 года</p></div>
         </div>
 
         <div class="Fred">
-            <img class="Fred_img" width="329" height="424" src="image/фред2.png" alt="">
+            <img class="Fred_img" width="329" height="424" src="../image/фред2.png" alt="">
             <div class="Fred_name"><p>Фред</p></div>
             <div class="Fred_age"><p>5 месяцев</p></div>
         </div>
 
         <div class="Dana">
-            <img class="Dana_img" width="598" height="693" src="image/дана1.png" alt="">
-            <div class="Dana_name"><p class="Dana_name">Дана</p></div>
+            <img class="Dana_img" width="598" height="693" src="../image/дана1.png" alt="">
+            <div class="Dana_name"><p>Дана</p></div>
             <div class="Dana_age"><p>4 месяца</p></div>
         </div>
 
         <div class="Kuzya">
-            <img class="Kuzya_img" width="634" height="678" src="image/кузя1.png" alt="">
+            <img class="Kuzya_img" width="634" height="678" src="../image/кузя1.png" alt="">
             <div class="Kuzya_name"><p>Кузя</p></div>
             <div class="Kuzya_age"><p>5 лет</p></div>
         </div>
 
         <div class="Martin">
-            <img class="Martin_img" width="329" height="424" src="image/мартин2.png" alt="">
+            <img class="Martin_img" width="329" height="424" src="../image/мартин2.png" alt="">
             <div class="Martin_name"><p>Мартин</p></div>
             <div class="Martin_age"><p>2 года</p></div>
         </div>
 
         <div class="Dick-Jack">
-            <img class="Dick-Jack_img" width="599" height="520" src="image/дик_джек3.png" alt="">
+            <img class="Dick-Jack_img" width="599" height="520" src="../image/дик_джек3.png" alt="">
             <div class="Dick-Jack_name"><p>Дик и Джек</p></div>
             <div class="Dick-Jack_age"><p>6 лет</p></div>
         </div>
 
         <div class="Butch">
-            <img class="Butch_img" width="456" height="500" src="image/буч1.png" alt="">
+            <img class="Butch_img" width="456" height="500" src="../image/буч1.png" alt="">
             <div class="Butch_name"><p>Буч</p></div>
             <div class="Butch_age"><p>4 месяца</p></div>
         </div>
 
         <div class="Lay">
-            <img class="Lay_img" width="598" height="693" src="image/лэя2.png" alt="">
+            <img class="Lay_img" width="598" height="693" src="../image/лэя2.png" alt="">
             <div class="Lay_name"><p>Лэя</p></div>
             <div class="Lay_age"><p>2 года</p></div>
         </div>
 
         <div class="Adele">
-            <img class="Adele_img" width="511" height="520" src="image/адель.png" alt="">
+            <img class="Adele_img" width="511" height="520" src="../image/адель.png" alt="">
             <div class="Adele_name"><p>Адель</p></div>
             <div class="Adele_age"><p>5 лет</p></div>
         </div>
 
         <div class="Ray">
-            <img class="Ray_img" width="484" height="561" src="image/рэй.png" alt="">
+            <img class="Ray_img" width="484" height="561" src="../image/рэй.png" alt="">
             <div class="Ray_name"><p>Рэй</p></div>
             <div class="Ray_age"><p>5 лет</p></div>
         </div>
 
         <div class="Batty">
-            <img class="Batty_img" width="598" height="562" src="image/бэтти.png" alt="">
+            <img class="Batty_img" width="598" height="562" src="../image/бэтти.png" alt="">
             <div class="Batty_name"><p>Бэтти</p></div>
             <div class="Batty_age"><p>4 года</p></div>
         </div>
@@ -106,7 +106,7 @@ require "db.php";
     </main>
 
     <footer class="footer">
-        <img class="footer_logo" src="image/f-logo.png" alt="Логотип">
+        <img class="footer_logo" src="../image/f-logo.png" alt="Логотип">
         <div class="Footer_menu">
             <div class="Footer_about"><p>О компании</p></div>
             <div class="Footer_news"><p>Новости</p></div>
@@ -123,11 +123,11 @@ require "db.php";
 
         <div class="Media">
             <p>Мы в сети</p>
-            <img class="Facebook" src="image/facebook.png" alt="">
-            <img class="Instagram" src="image/instagram.png" alt="">
-            <img class="Vk" src="image/vk.png" alt="">
-            <img class="Youtube" src="image/youtube.png" alt="">
-            <img class="Twitter" src="image/twitter.png" alt="">
+            <img class="Facebook" src="../image/facebook.png" alt="">
+            <img class="Instagram" src="../image/instagram.png" alt="">
+            <img class="Vk" src="../image/vk.png" alt="">
+            <img class="Youtube" src="../image/youtube.png" alt="">
+            <img class="Twitter" src="../image/twitter.png" alt="">
         </div>
         <div class="year"><p>2017-2022</p></div>
     </footer>
